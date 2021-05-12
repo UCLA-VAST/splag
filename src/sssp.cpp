006f280046ed63873453169f087212899bb094df
Blaok <i@blaok.me>
2021-05-12 15:44:38 -0700

    refactor(soc): reduce switch latency & area

--- src/sssp.cpp
+++ src/sssp.cpp
@@ -1170,12 +1170,57 @@ void Switch(
     istream<TaskOnChip>& in_q0, istream<TaskOnChip>& in_q1,
     //
     ostream<TaskOnChip>& out_q0, ostream<TaskOnChip>& out_q1) {
-  streams<TaskOnChip, 4, 8> buffer;
-  tapa::task()
-      .invoke<detach>(VidDemux, b, in_q0, buffer[0], buffer[1])
-      .invoke<detach>(VidDemux, b, in_q1, buffer[2], buffer[3])
-      .invoke<detach>(VidMux, buffer[0], buffer[2], out_q0)
-      .invoke<detach>(VidMux, buffer[1], buffer[3], out_q1);
+  bool should_prioritize_1 = false;
+spin:
+  for (bool is_pkt_0_valid, is_pkt_1_valid;;) {
+#pragma HLS pipeline II = 1
+#pragma HLS latency max = 0
+    const auto pkt_0 = in_q0.peek(is_pkt_0_valid);
+    const auto pkt_1 = in_q1.peek(is_pkt_1_valid);
+
+    const uint_vid_t addr_0 = pkt_0.vid();
+    const uint_vid_t addr_1 = pkt_1.vid();
+
+    const bool should_fwd_0_0 = is_pkt_0_valid && !addr_0.get_bit(b);
+    const bool should_fwd_0_1 = is_pkt_0_valid && addr_0.get_bit(b);
+    const bool should_fwd_1_0 = is_pkt_1_valid && !addr_1.get_bit(b);
+    const bool should_fwd_1_1 = is_pkt_1_valid && addr_1.get_bit(b);
+
+    const bool has_conflict = is_pkt_0_valid && is_pkt_1_valid &&
+                              should_fwd_0_0 == should_fwd_1_0 &&
+                              should_fwd_0_1 == should_fwd_1_1;
+
+    const bool should_read_0 = !((!should_fwd_0_0 && !should_fwd_0_1) ||
+                                 (should_prioritize_1 && has_conflict));
+    const bool should_read_1 = !((!should_fwd_1_0 && !should_fwd_1_1) ||
+                                 (!should_prioritize_1 && has_conflict));
+    const bool should_write_0 = should_fwd_0_0 || should_fwd_1_0;
+    const bool should_write_1 = should_fwd_1_1 || should_fwd_0_1;
+    const bool shoud_write_0_0 =
+        should_fwd_0_0 && (!should_fwd_1_0 || !should_prioritize_1);
+    const bool shoud_write_1_1 =
+        should_fwd_1_1 && (!should_fwd_0_1 || should_prioritize_1);
+
+    // if can forward through (0->0 or 1->1), do it
+    // otherwise, check for conflict
+    const bool is_0_written =
+        should_write_0 && out_q0.try_write(shoud_write_0_0 ? pkt_0 : pkt_1);
+    const bool is_1_written =
+        should_write_1 && out_q1.try_write(shoud_write_1_1 ? pkt_1 : pkt_0);
+
+    // if can forward through (0->0 or 1->1), do it
+    // otherwise, round robin priority of both ins
+    if (should_read_0 && (shoud_write_0_0 ? is_0_written : is_1_written)) {
+      in_q0.read(nullptr);
+    }
+    if (should_read_1 && (shoud_write_1_1 ? is_1_written : is_0_written)) {
+      in_q1.read(nullptr);
+    }
+
+    if (has_conflict) {
+      should_prioritize_1 = !should_prioritize_1;
+    }
+  }
 }
 
 void EdgeReqArbiter(tapa::istreams<EdgeReq, kPeCount>& req_q,
