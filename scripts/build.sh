0c7292761cfffaeb433a2f6a832e77e622aec5b6
Blaok <i@blaok.me>
2021-12-13 22:18:21 -0800

    chore: add build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+base_dir="$(realpath "${0%/*}")"
+build_dir="$(realpath "$1")"
+
+# Hack v++ so that it exits clean.
+tmp_dir=/var/tmp/"${USER}"/bin
+mkdir -p "${tmp_dir}"
+cat >"${tmp_dir}"/v++ <<'EOF'
+#!/bin/bash
+source "${XILINX_VITIS}"/settings64.sh
+cleanup() { kill -9 $(ps -s "${pid}" -o pid=) >/dev/null 2>/dev/null; }
+setsid "$(basename "$0")" "$@" &
+pid=$!
+trap cleanup SIGINT SIGTERM SIGPIPE SIGCHLD EXIT
+wait
+EOF
+chmod +x "${tmp_dir}"/v++
+PATH="${tmp_dir}:${PATH}"
+
+cmake -S. -B"${build_dir}" -DCMAKE_BUILD_TYPE=Release
+make -C "${build_dir}" sssp-hw-xo
+bash <<EOF
+exec make -C ${build_dir@Q} SSSP.xilinx_u280_xdma_201920_3.hw_xclbin
+EOF
